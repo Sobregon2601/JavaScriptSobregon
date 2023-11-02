2773210c70954c7d310c420f5e6e960841ccbc8c
Mathias Obregon <obregonmathias@hotmail.com>
2023-11-01 23:51:01 -0300

    java

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+
+///nicks usuarios que acceden : profesor1 , tutor1 y tutor 2///
+
+//let identificar = true
+//let intentos = 1//
+
+//do{//
+
+let usuarioPagina = prompt("ingresa tu nick de usuario").toLowerCase()
+
+//let profesor1 = "lautaro"//
+
+//if(usuario=== null){
+//
+
+if(usuarioPagina == "profesor1"){
+    alert ("bienvenido "+ " " + "lautaro")
+}else if(usuarioPagina=="tutor1"){
+    alert ("bienvenido" + " " + "ramiro")
+}else if(usuarioPagina == "tutor2"){
+    alert ("bienvenido" + " " + "profe")
+}else{
+    alert("esta pagina es solo para profesores/tutores")
+}
+//}while(identificar)//
